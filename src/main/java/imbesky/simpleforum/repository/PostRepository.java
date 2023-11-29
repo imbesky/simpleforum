839bec6caf9ebc3958d7a80f4853917902e550e8
besky <onlybesky@gmail.com>
2023-11-29 18:59:35 +0900

    chore: removed unused import statement

--- src/main/java/imbesky/simpleforum/repository/PostRepository.java
+++ src/main/java/imbesky/simpleforum/repository/PostRepository.java
@@ -1,7 +1,6 @@
 package imbesky.simpleforum.repository;
 
 import imbesky.simpleforum.domain.Post;
-import imbesky.simpleforum.domain.dto.PostEditDto;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 
